3f0927ffc55a15f3452e5115b2a038b8e7a5dc09
Justin McMillion <jmcmillion89@icloud.com>
2024-10-16 18:42:33 -0400

    fix background video

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
       className="hero"
     >
       <video autoPlay loop muted playsInline className="hero-video">
-        <source src="./public/background.mp4" type="video/mp4" />
+        <source src="background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
